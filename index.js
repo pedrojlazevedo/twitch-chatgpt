4d6b1356ba24d76c56a1811de6c98109210dfc88
Pedro Azevedo <pedrojose.azevedo@farfetch.com>
2023-02-22 13:12:06 +0000

    Update index.js

--- index.js
+++ index.js
@@ -7,8 +7,14 @@ app.all('/', (req, res) => {
 })
 
 app.all('/rank', (req, res) => {
-  console.log("Test")
-  res.send("Perfect <3")
+    console.log("Test")
+    res.send("Perfect <3")
+})
+
+app.get('/rank', (req, res) => {
+    console.log(req.query)
+    const name = req.query.name
+    res.send('name: ' + req.query.name)
 })
 
 app.listen(process.env.PORT || 3000)
