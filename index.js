62d59ebc85dc4f32e2636e7ea01db3e8f96d077f
Pedro Azevedo <pedrojose.azevedo@farfetch.com>
2023-02-22 13:07:17 +0000

    feat: add rank testing

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const express = require('express')
 const app = express()
+
 app.all('/', (req, res) => {
     console.log("Just got a request!")
     res.send('Yo!')
 })
+
+app.all('/rank', (req, res) => {
+  console.log("Test")
+  res.send("Perfect <3")
+})
+
 app.listen(process.env.PORT || 3000)
