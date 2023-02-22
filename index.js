70f3cbe59f6ec314a85275b373ee3a0f71381fe5
Pedro Azevedo <pedrojose.azevedo@farfetch.com>
2023-02-22 13:17:21 +0000

    feat: add name possibility

--- index.js
+++ index.js
@@ -11,10 +11,15 @@ app.all('/rank', (req, res) => {
     res.send("Perfect <3")
 })
 
-app.get('/rank', (req, res) => {
-    console.log(req.query)
-    const name = req.query.name
-    res.send('name: ' + req.query.name)
+app.get('/rank/:name', (req, res) => {
+    res.send('user:' + req.params.name);    
+    //const res = $(urlfetch json "https://aoe4world.com/api/v0/players/search?query=$(querystring)");
+    //if (res.players.length===0){const res = $(urlfetch json "https://aoe4world.com/api/v0/players/search?query=GT_Mjerticla";
+    //    `${res.players[0].name} - [Solo] ${res.players[0].leaderboards.rm_solo.rank_level} - ${res.players[0].leaderboards.rm_solo.rating}`}
+    //else if (res.players[0].leaderboards.rm_solo) {`${res.players[0].name} - [Solo] ${res.players[0].leaderboards.rm_solo.rank_level} - ${res.players[0].leaderboards.rm_solo.rating}`}
+    //else if (res.players[0].leaderboards.rm_team) {`${res.players[0].name} - [Team] ${res.players[0].leaderboards.rm_team.rank_level} - ${res.players[0].leaderboards.rm_team.rating}`}
+    //else {`There is no information about player ${res.players[0].name}`}
+    //)
 })
 
 app.listen(process.env.PORT || 3000)
