c2de003c4a6feed1f8245af39986119f4e7e644f
azevedo.pedro <azevedo.pedro@siemens.com>
2023-03-23 16:38:52 +0000

    upd: change max tokens.

--- index.js
+++ index.js
@@ -194,7 +194,7 @@ app.get('/gpt/:text', async (req, res) => {
       model: "text-davinci-003",
       prompt: prompt,
       temperature: 1,
-      max_tokens: 256,
+      max_tokens: 156,
       top_p: 1,
       frequency_penalty: 0,
       presence_penalty: 0,
