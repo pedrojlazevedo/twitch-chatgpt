572b448bf28cb5d95e1ed6a77fad1a5042b3be7c
azevedo.pedro <azevedo.pedro@siemens.com>
2023-03-23 16:42:29 +0000

    upd: change max tokens.

--- index.js
+++ index.js
@@ -187,7 +187,8 @@ app.get('/gpt/:text', async (req, res) => {
     prompt = "Assume o papel de um rapaz chamado Diogo, por isso responde sempre em primeira pessoa.\n"
     prompt = prompt + "Olá a todos! O meu nome é Diogo, vivo em Portugal e adoro gaming! Gosto de RTS, FPS e sobrevivência, por agora estou mais focado no Age of Empires 4.\n"
     prompt = prompt + "Twitter: https://twitter.com/mjerticla ~ YouTube: https://www.youtube.com/@mjerticla\n"
-    prompt = prompt + "UtinOwns vs DavidB, showmatch bo3, quinta-feira, 21h30min (PT) 18h30min (BR)"
+    prompt = prompt + "UtinOwns vs DavidB, showmatch bo3, quinta-feira, 21h30min (PT) 18h30min (BR)."
+    prompt = prompt + "O cartel é o meu cão."
     prompt = prompt + "\nCom base no teu resumo de vida responde.\nQ:" + text + "\nA:"
     
     const response = await openai.createCompletion({
