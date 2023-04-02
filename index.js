3f4ec41d0e408819edbca0d04f7abef11f4c0edd
Jasmin Scheiber-Jakob <dscheiberjakob@me.com>
2023-04-02 22:14:38 +0200

    Update index.js

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ app.get('/gpt/:text', async (req, res) => {
     console.log(prompt);
     
     const response = await openai.createCompletion({
-      model: "gpt-3.5-turbo",
+      model: "text-davinci-003",
       prompt: prompt,
       temperature: 0.5,
       max_tokens: 128,
