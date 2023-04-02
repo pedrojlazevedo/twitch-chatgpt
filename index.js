17a9c77f192bae108086141b3915d38ca7b919ad
Jasmin Scheiber-Jakob <dscheiberjakob@me.com>
2023-04-02 23:54:01 +0200

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,14 @@ const fs = require('fs');
 const { promisify } = require('util')
 const readFile = promisify(fs.readFile)
 
+
+const removeLines = (data, lines = []) => {
+    return data
+        .split('\n')
+        .filter((val, idx) => lines.indexOf(idx) === -1)
+        .join('\n');
+}
+
 app.use(express.json({extended: true, limit: '1mb'}))
 
 
@@ -20,6 +28,7 @@ fs.readFile("./file_context.txt", 'utf8', function(err, data) {
   file_context = data;
 });
 
+let chat_history =""
 app.get('/gpt/:text', async (req, res) => {
     const text = req.params.text
     const { Configuration, OpenAIApi } = require("openai");
@@ -29,8 +38,33 @@ app.get('/gpt/:text', async (req, res) => {
       apiKey: process.env.OPENAI_API_KEY,
     });
     const openai = new OpenAIApi(configuration);
+    
+    //extend chat history file
+    fs.appendFile('./chat_history.txt', text + "\n", function (err) {
+        if (err) {
+            throw err;
+        } else {
+            
+        }
+    })
+    // read chat history file
+    fs.readFile("./chat_history.txt", 'utf8', function(err, data) {
+        if (err) throw err;
+        chat_history = data;
+    })
+    
+    fs.readFile("./chat_history.txt", 'utf8', (err, data) => {
+    if (err) throw err;
 
-    const prompt = file_context + "\n\nQ:" + text + "\nA:";
+    // remove the first line and the 5th and 6th lines in the file
+    fs.writeFile("./chat_history.txt", removeLines(data, [10]), 'utf8', function(err) {
+        if (err) throw err;
+        console.log("the lines have been removed.");
+    });
+})
+                
+   
+    const prompt = file_context + "\n Hier ist der Verlauf der letzten Nachrichten von den Chatteilnehmern mit dir: \n"+ chat_history + "\n\nQ:" + text + "\nA:";
     console.log(prompt);
     
     const response = await openai.createCompletion({
