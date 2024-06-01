8a62020bd27efc60641c5545599a589fb6e40bcb
pedrojlazevedo <pedro6@gmail.com>
2024-06-01 01:02:20 +0100

    fix: nightbot command not working on render

--- index.js
+++ index.js
@@ -158,6 +158,7 @@ if (GPT_MODE === 'CHAT') {
 
 app.get('/gpt/:text', async (req, res) => {
   const text = req.params.text;
+  const channel = channels[0];
   const answerQuestion = async answer => {
     if (answer.length > maxLength) {
       const messages = answer.match(new RegExp(`.{1,${maxLength}}`, 'g'));
